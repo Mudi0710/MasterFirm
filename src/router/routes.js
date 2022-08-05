4ff074829ed8cd22ecf042edfcf172b493673b33
Mudi0710 <nomis820710@gmail.com>
2022-08-05 16:29:03 +0800

    修改-src/router/routes.js：修改路由
    修改路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,57 @@
+import FrontLayout from '@/layouts/MainLayout.vue'
+import FrontHomeView from '@/pages/front/IndexPage.vue'
 
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: FrontLayout,
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      {
+        path: '',
+        name: 'home',
+        component: FrontHomeView,
+        meta: {
+          title: '法師事務所',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'register',
+        name: 'register',
+        component: () => import('@/pages/front/RegisterView.vue'),
+        meta: {
+          title: '法師事務所 | 註冊',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'login',
+        name: 'login',
+        component: () => import('@/pages/front/LoginView.vue'),
+        meta: {
+          title: '法師事務所 | 登入',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'cart',
+        name: 'cart',
+        component: () => import('@/layouts/CartLayout.vue'),
+        meta: {
+          title: '法師事務所 | 購物車',
+          login: true,
+          admin: false
+        }
+      }
     ]
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: () => import('layouts/MainLayout.vue')
   },
 
   // Always leave this as last one,
@@ -17,3 +63,23 @@ const routes = [
 ]
 
 export default routes
+
+// 原始碼
+// const routes = [
+//   {
+//     path: '/',
+//     component: () => import('layouts/MainLayout.vue'),
+//     children: [
+//       { path: '', component: () => import('pages/IndexPage.vue') }
+//     ]
+//   },
+
+//   // Always leave this as last one,
+//   // but you can also remove it
+//   {
+//     path: '/:catchAll(.*)*',
+//     component: () => import('pages/ErrorNotFound.vue')
+//   }
+// ]
+
+// export default routes
