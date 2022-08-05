a7c6bcc8461a2e6a5570817422b2af5236019349
Mudi0710 <nomis820710@gmail.com>
2022-08-05 16:30:57 +0800

    建立-src/pages/front/RegisterView.vue：建立註冊頁面
    建立註冊頁面

---
+++ src/pages/front/RegisterView.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <br>
+    註冊
+  </q-page>
+</template>
