30dab901522e93776107ec4fa6c9de04453ae4fc
Mudi0710 <nomis820710@gmail.com>
2022-08-09 16:00:12 +0800

    修改-src/layouts/MainLayout.vue：修改初始 Layout
    修改初始 Layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,9 @@
     <q-header class="bg-primary text-secondary lt-xl q-px-md">
       <q-toolbar>
         <q-toolbar-title>
-          法師事務所
+          <router-link to="/">
+            法師事務所
+          </router-link>
         </q-toolbar-title>
         <!-- 漢堡 -->
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
@@ -12,16 +14,16 @@
 
     <q-drawer v-model="leftDrawerOpen" side="left" :breakpoint="1213" show-if-above :width="150"
       class="bg-primary text-secondary text-center q-py-lg q-px-md column justify-center" style="overflow: visible;">
-      <a href="#" class="col-2">
+      <router-link to="/" class="col-2">
         <div class="text-h3 master">法師</div>
         <div class="text-h4">事務所</div>
-      </a>
+      </router-link>
       <q-list class="col-5">
         <!-- v-ripple 點擊時有波紋特效 -->
         <q-item clickable v-ripple dense>
           <q-item-section>預約諮詢</q-item-section>
         </q-item>
-        <q-separator dark />
+        <q-separator />
         <q-item clickable v-ripple dense>
           <q-item-section>本所簡介</q-item-section>
         </q-item>
@@ -44,15 +46,66 @@
       </q-list>
 
       <q-list class="col-5 text-h5">
-        <q-btn round flat icon="fa-brands fa-square-facebook" /><br>
-        <q-btn round flat icon="fa-brands fa-instagram" /><br>
-        <q-btn round flat icon="fa-brands fa-line" /><br>
-        <q-btn round flat icon="fa-solid fa-user-plus" to="register" /><br>
-        <q-btn round flat icon="fa-solid fa-right-to-bracket" to="login" /><br>
-        <q-btn round flat icon="fa-solid fa-address-card" to="#" /><br>
-        <q-btn round flat icon="fa-regular fa-calendar-days" to="#" /><br>
-        <q-btn round flat icon="fa-solid fa-receipt" to="#" /><br>
-        <q-btn round flat icon="fa-solid fa-right-from-bracket" /><br>
+        <q-btn round flat icon="fa-brands fa-square-facebook">
+          <q-tooltip transition-show="fade" transition-hide="fade" anchor="center right" self="center left" :offset="[-5, 0]">
+            Facebook
+          </q-tooltip>
+        </q-btn>
+        <br>
+        <q-btn round flat icon="fa-brands fa-instagram">
+          <q-tooltip transition-show="fade" transition-hide="fade" anchor="center right" self="center left" :offset="[-5, 0]">
+            Instagram
+          </q-tooltip>
+        </q-btn>
+        <br>
+        <q-btn round flat icon="fa-brands fa-line">
+          <q-tooltip transition-show="fade" transition-hide="fade" anchor="center right" self="center left" :offset="[-5, 0]">
+            Line
+          </q-tooltip>
+        </q-btn>
+        <br>
+        <q-btn round flat icon="fa-solid fa-user-plus" to="register">
+          <q-tooltip transition-show="fade" transition-hide="fade" anchor="center right" self="center left" :offset="[-5, 0]">
+            註冊
+          </q-tooltip>
+        </q-btn>
+        <br>
+        <q-btn round flat icon="fa-solid fa-right-to-bracket" to="login">
+          <q-tooltip transition-show="fade" transition-hide="fade" anchor="center right" self="center left" :offset="[-5, 0]">
+            登入
+          </q-tooltip>
+        </q-btn>
+        <br>
+        <q-btn round flat icon="fa-solid fa-address-card" to="#">
+          <q-tooltip transition-show="fade" transition-hide="fade" anchor="center right" self="center left" :offset="[-5, 0]">
+            會員資料
+          </q-tooltip>
+        </q-btn>
+        <br>
+        <q-btn round flat icon="fa-regular fa-calendar-days" to="#">
+          <q-tooltip transition-show="fade" transition-hide="fade" anchor="center right" self="center left" :offset="[-5, 0]">
+            預約查詢
+          </q-tooltip>
+        </q-btn>
+        <br>
+        <q-btn round flat icon="fa-solid fa-receipt" to="#">
+          <q-tooltip transition-show="fade" transition-hide="fade" anchor="center right" self="center left" :offset="[-5, 0]">
+            訂單查詢
+          </q-tooltip>
+        </q-btn>
+        <br>
+        <q-btn round dense flat icon="fa-solid fa-cart-shopping">
+          <q-tooltip transition-show="fade" transition-hide="fade" anchor="center right" self="center left" :offset="[-5, 0]">
+            購物車
+          </q-tooltip>
+        </q-btn>
+        <br>
+        <q-btn round flat icon="fa-solid fa-right-from-bracket">
+          <q-tooltip transition-show="fade" transition-hide="fade" anchor="center right" self="center left" :offset="[-5, 0]">
+            登出
+          </q-tooltip>
+        </q-btn>
+        <br>
         <!-- <q-fab v-model="fab2" icon="fa-solid fa-user" direction="right" flat q-gutter-lg>
           <q-fab-action flat external-label label-position="top" @click="onClick" icon="fa-solid fa-user-plus" label="註冊" to="register" label-style="background-color: rgba(0,0,0,0);" label-class="text-secondary text-subtitle1" />
           <q-fab-action flat external-label label-position="top" @click="onClick" icon="fa-solid fa-right-to-bracket" label="登入" to="login" label-style="background-color: rgba(0,0,0,0);" label-class="text-secondary text-subtitle1" />
@@ -61,9 +114,7 @@
           <q-fab-action flat external-label label-position="top" @click="onClick" icon="fa-solid fa-receipt" label="訂單查詢" to="#" label-style="background-color: rgba(0,0,0,0);" label-class="text-secondary text-subtitle1" />
           <q-fab-action flat external-label label-position="top" @click="onClick" icon="fa-solid fa-right-from-bracket" label="登出" to="#" label-style="background-color: rgba(0,0,0,0);" label-class="text-secondary text-subtitle1" />
         </q-fab><br> -->
-        <q-btn round dense flat icon="fa-solid fa-cart-shopping" />
       </q-list>
-
     </q-drawer>
 
     <q-page-container>
