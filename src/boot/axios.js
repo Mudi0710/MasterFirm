419b8fe704f9b34af6d2933a7ad8f6a0f28edaff
Mudi0710 <nomis820710@gmail.com>
2022-08-10 16:28:00 +0800

    修改-src/boot/axios.js：修改 axios
    修改 axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,5 +1,7 @@
 import { boot } from 'quasar/wrappers'
+
 import axios from 'axios'
+import { useUserStore } from '@/stores/user'
 
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
@@ -18,6 +20,54 @@ import axios from 'axios'
 */
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API
+})
+
+/*
+  與上面的 post 相同，由於每個與 api 溝通的地方都要重複寫
+  因此換個寫法，自訂一個自己的預設 axios 設定
+  2022/07/11 2:45:30
+*/
+export const apiAuth = axios.create({
+  baseURL: import.meta.env.VITE_API
+})
+
+/* axios ---> axios 攔截請求(req) --> API SERVER --> axios 攔截回應(res) --> 呼叫的地方 */
+
+// axios 攔截請求，加上JWT
+apiAuth.interceptors.request.use(config => {
+  const user = useUserStore()
+  config.headers.authorization = `Bearer ${user.token}`
+  return config
+})
+
+// axios 攔截回應，加上JWT
+apiAuth.interceptors.response.use(res => {
+  return res
+}, error => {
+  // 如果請求有回應
+  if (error.response) {
+    // 如果是 401，可能是 JWT 過期
+    if (error.response.status === 401) {
+      // 確認原始請求的網址不是延長登入，才重新登入
+      if (error.config.url !== '/users/extend' && error.config.url !== '/users/logout') {
+        const user = useUserStore()
+        // 傳送延長請求
+        return apiAuth.post('/users/extend', {}).then(({ data }) => {
+          // 更新 JWT
+          user.token = data.result
+          // 使用新的 JWT 再次嘗試原始請求
+          error.config.headers.authorization = `Bearer ${user.token}`
+          return axios(error.config)
+        }).catch(_ => {
+          // 重新登入失敗，強制登出
+          user.logout()
+          // 回傳延長登入請求的錯誤訊息到呼叫的地方
+          return Promise.reject(error)
+        })
+      }
+    }
+  }
+  return Promise.reject(error)
 })
 
 export default boot(({ app }) => {
