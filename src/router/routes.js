8e33f707486b709040ddfee051046fda7c0252b5
Mudi0710 <nomis820710@gmail.com>
2022-08-11 16:32:30 +0800

    修改-src/router/routes.js：修改路由
    修改路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,7 @@
 import FrontLayout from '@/layouts/MainLayout.vue'
 import FrontHomeView from '@/pages/front/IndexPage.vue'
+import AdminLayout from '@/layouts/AdminLayout.vue'
+import NotFoundView from '@/pages/ErrorNotFound.vue'
 
 const routes = [
   {
@@ -51,14 +53,87 @@ const routes = [
   {
     path: '/admin',
     name: 'admin',
-    component: () => import('layouts/MainLayout.vue')
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'admin-home',
+        component: () => import('@/pages/admin/AdminHomeView.vue'),
+        meta: {
+          title: '法師事務所 | 管理後台',
+          login: true,
+          admin: true
+        }
+      },
+      {
+        path: 'adminMember',
+        name: 'admin-member',
+        component: () => import('@/pages/admin/AdminMemberView.vue'),
+        meta: {
+          title: '法師事務所 | 會員管理',
+          login: true,
+          admin: true
+        }
+      },
+      {
+        path: 'adminCase',
+        name: 'admin-case',
+        component: () => import('@/pages/admin/AdminCaseView.vue'),
+        meta: {
+          title: '法師事務所 | 預約管理',
+          login: true,
+          admin: true
+        }
+      },
+      {
+        path: 'adminOrder',
+        name: 'admin-order',
+        component: () => import('@/pages/admin/AdminOrderView.vue'),
+        meta: {
+          title: '法師事務所 | 訂單管理',
+          login: true,
+          admin: true
+        }
+      },
+      {
+        path: 'adminArticle',
+        name: 'admin-article',
+        component: () => import('@/pages/admin/AdminArticleView.vue'),
+        meta: {
+          title: '法師事務所 | 文章管理',
+          login: true,
+          admin: true
+        }
+      },
+      {
+        path: 'adminPromote',
+        name: 'admin-promote',
+        component: () => import('@/pages/admin/AdminPromoteView.vue'),
+        meta: {
+          title: '法師事務所 | 推播管理',
+          login: true,
+          admin: true
+        }
+      }
+    ]
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
+    path: '/404',
+    name: '404',
+    component: NotFoundView,
+    meta: {
+      title: '法師事務所 | 404',
+      login: false,
+      admin: false
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'all',
+    redirect: '/404'
   }
 ]
 
