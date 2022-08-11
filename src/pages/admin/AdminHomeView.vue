2d44e337bafa7792b6e64bb2faf68a7f20ce547b
Mudi0710 <nomis820710@gmail.com>
2022-08-11 16:33:22 +0800

    建立-src/pages/admin/AdminHomeView.vue：建立管理後台頁面
    建立管理後台頁面

---
+++ src/pages/admin/AdminHomeView.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page-container>
+    <q-page class="column">
+      <div id="myheader"></div>
+      <div id="adminHome">
+        <div class="q-pa-md row">
+          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
+            <q-card-section>管理後台</q-card-section>
+          </q-card>
+          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
+            <q-card-section>
+              <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" basic> 121</q-img>
+            </q-card-section>
+          </q-card>
+          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
+            <q-card-section>
+              <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" basic> 121</q-img>
+            </q-card-section>
+          </q-card>
+          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
+            <q-card-section>
+              <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" basic> 121</q-img>
+            </q-card-section>
+          </q-card>
+          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
+            <q-card-section>
+              <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" basic> 121</q-img>
+            </q-card-section>
+          </q-card>
+          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
+            <q-card-section>
+              <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" basic> 121</q-img>
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+    </q-page>
+  </q-page-container>
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+
+const slide = ref(1)
+const autoplay = ref(true)
+
+</script>
