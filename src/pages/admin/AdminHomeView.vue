c28053bbc05de495f2df9751aebe9b8eb8d58011
Mudi0710 <nomis820710@gmail.com>
2022-08-12 00:38:44 +0800

    修改-src/pages/admin/AdminHomeView.vue：修改管理後台頁面
    修改管理後台頁面

--- src/pages/admin/AdminHomeView.vue
+++ src/pages/admin/AdminHomeView.vue
@@ -1,41 +1,30 @@
 <template>
-  <q-page-container>
-    <q-page class="column">
-      <div id="myheader"></div>
-      <div id="adminHome">
-        <div class="q-pa-md row">
-          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
-            <q-card-section>管理後台</q-card-section>
-          </q-card>
-          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
-            <q-card-section>
-              <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" basic> 121</q-img>
-            </q-card-section>
-          </q-card>
-          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
-            <q-card-section>
-              <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" basic> 121</q-img>
-            </q-card-section>
-          </q-card>
-          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
-            <q-card-section>
-              <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" basic> 121</q-img>
-            </q-card-section>
-          </q-card>
-          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
-            <q-card-section>
-              <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" basic> 121</q-img>
-            </q-card-section>
-          </q-card>
-          <q-card square flat class="my-card col-12 col-sm-6 col-xl-4">
-            <q-card-section>
-              <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" basic> 121</q-img>
-            </q-card-section>
-          </q-card>
-        </div>
+  <q-page>
+    <div id="adminHome" class="text-secondary">
+      <div id="adminHomeHeader"></div>
+      <div class="adminHomeLayout q-pa-md row justify-center">
+        <q-card square flat class="my-card col-6 col-sm-6 col-xl-4 bg-primary text-center row">
+          <div class="col-12 self-center text-h3 title">管理<br class="br">後台</div>
+        </q-card>
+        <q-card square flat class="my-card col-6 col-sm-6 col-xl-4 bg-primary text-center row">
+          <div class="col-12 self-center text-h3 title">會員<br class="br">管理</div>
+        </q-card>
+        <q-card square flat class="my-card col-6 col-sm-6 col-xl-4 bg-primary text-center row">
+          <div class="col-12 self-center text-h3 title">預約<br class="br">管理</div>
+        </q-card>
+        <q-card square flat class="my-card col-6 col-sm-6 col-xl-4 bg-primary text-center row">
+          <div class="col-12 self-center text-h3 title">訂單<br class="br">管理</div>
+        </q-card>
+        <q-card square flat class="my-card col-6 col-sm-6 col-xl-4 bg-primary text-center row">
+          <div class="col-12 self-center text-h3 title">文章<br class="br">管理</div>
+        </q-card>
+        <q-card square flat class="my-card col-6 col-sm-6 col-xl-4 bg-primary text-center row">
+          <div class="col-12 self-center text-h3 title">推播<br class="br">管理</div>
+        </q-card>
       </div>
-    </q-page>
-  </q-page-container>
+
+    </div>
+  </q-page>
 </template>
 
 <script setup>
