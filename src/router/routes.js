feeb107c66c6bed66a57ee538f364a76dc292490
Mudi0710 <nomis820710@gmail.com>
2022-08-16 16:32:23 +0800

    修改-src/router/routes.js：修改路由
    修改路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -49,6 +49,136 @@ const routes = [
         }
       },
       {
+        path: 'Appointment',
+        name: 'Appointment',
+        component: () => import('@/pages/front/AppointmentView.vue'),
+        meta: {
+          title: '法師事務所 | 預約諮詢',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'Introduction',
+        name: 'Introduction',
+        component: () => import('@/pages/front/IntroductionView.vue'),
+        meta: {
+          title: '法師事務所 | 本所簡介',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'News',
+        name: 'News',
+        component: () => import('@/pages/front/NewsView.vue'),
+        meta: {
+          title: '法師事務所 | 最新消息總覽',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'New/:id',
+        name: 'New',
+        component: () => import('@/pages/front/NewView.vue'),
+        meta: {
+          title: '法師事務所 | 最新消息',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'Services',
+        name: 'Services',
+        component: () => import('@/pages/front/ServicesView.vue'),
+        meta: {
+          title: '法師事務所 | 服務項目總覽',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'Service/:id',
+        name: 'Service',
+        component: () => import('@/pages/front/ServiceView.vue'),
+        meta: {
+          title: '法師事務所 | 服務項目',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'Knowledges',
+        name: 'Knowledges',
+        component: () => import('@/pages/front/KnowledgesView.vue'),
+        meta: {
+          title: '法師事務所 | 靈學知識總覽',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'Knowledge/:id',
+        name: 'Knowledge',
+        component: () => import('@/pages/front/KnowledgeView.vue'),
+        meta: {
+          title: '法師事務所 | 靈學知識',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'Cases',
+        name: 'Cases',
+        component: () => import('@/pages/front/CasesView.vue'),
+        meta: {
+          title: '法師事務所 | 案例分享總覽',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'Case/:id',
+        name: 'Case',
+        component: () => import('@/pages/front/CaseView.vue'),
+        meta: {
+          title: '法師事務所 | 案例分享',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'Products',
+        name: 'Products',
+        component: () => import('@/pages/front/ProductsView.vue'),
+        meta: {
+          title: '法師事務所 | 開運小物總覽',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'Product/:id',
+        name: 'Product',
+        component: () => import('@/pages/front/ProductView.vue'),
+        meta: {
+          title: '法師事務所 | 開運小物',
+          login: false,
+          admin: false
+        }
+      },
+      {
+        path: 'Content',
+        name: 'Content',
+        component: () => import('@/pages/front/ContentView.vue'),
+        meta: {
+          title: '法師事務所 | 聯絡我們',
+          login: false,
+          admin: false
+        }
+      },
+      {
         path: 'member',
         name: 'member',
         component: () => import('@/pages/front/MemberView.vue'),
