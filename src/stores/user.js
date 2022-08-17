9675e0d1e9bd4e821ba21a6d496fc51307b9d5e6
Mudi0710 <nomis820710@gmail.com>
2022-08-17 16:28:51 +0800

    修改-src/stores/user.js：修改 stores/user.js
    修改 stores/user.js

--- src/stores/user.js
+++ src/stores/user.js
@@ -9,7 +9,9 @@ import Swal from 'sweetalert2'
   2022/07/11 1:35:14
 
   BUT!! 在 Quasar 寫法又不一樣
+  => 不需要引入 router，直接在 function 將 router 前面加上 this. 就可以了
 */
+// import router from '@/router'
 
 export const useUserStore = defineStore({
   id: 'user',
@@ -62,7 +64,7 @@ export const useUserStore = defineStore({
         })
       }
     },
-    async logout () {
+    async logout() {
       try {
         // 設定新的 axios 實體，並加上 JWT
         await apiAuth.delete('/users/logout')
@@ -79,6 +81,86 @@ export const useUserStore = defineStore({
       this.name = ''
       this.role = 0
       this.cart = 0
+    },
+    async addCart(data) {
+      if (this.token.length === 0) {
+        Swal.fire({
+          icon: 'error',
+          title: '加入失敗',
+          text: '請先登入會員，再使用購物車功能。'
+        })
+        this.router.push('/login')
+        return
+      }
+      if (data.quantity <= 0) {
+        Swal.fire({
+          icon: 'error',
+          title: '加入失敗',
+          text: '訂購數量必須大於 0'
+        })
+        return
+      }
+      try {
+        const { data: resData } = await apiAuth.post('/users/cart', data)
+        this.cart = resData.result
+        Swal.fire({
+          icon: 'success',
+          title: '加入成功',
+          text: '商品已加入購物車！'
+        })
+      } catch (error) {
+        Swal.fire({
+          icon: 'error',
+          title: '加入失敗',
+          text: '商品無法加入購物車！'
+        })
+      }
+    },
+    async updateCart(data) {
+      try {
+        await apiAuth.patch('/users/cart', data)
+        return true
+      } catch (error) {
+        Swal.fire({
+          icon: 'error',
+          title: '失敗',
+          text: '更新購物車失敗！'
+        })
+        return false
+      }
+    },
+    async checkout() {
+      try {
+        await apiAuth.post('/orders')
+        this.cart = 0
+        Swal.fire({
+          icon: 'success',
+          title: '成功',
+          text: '結帳成功，謝謝您的購買！'
+        })
+        this.router.push('/order')
+      } catch (error) {
+        Swal.fire({
+          icon: 'error',
+          title: '失敗',
+          text: '結帳失敗，請稍後再嘗試。'
+        })
+      }
+    },
+    async getUser() {
+      if (this.token.length === 0) return
+      try {
+        const { data } = await apiAuth.get('/users')
+        this.account = data.result.account
+        this.role = data.result.role
+        this.cart = data.result.cart
+      } catch (error) {
+        this.logout()
+      }
     }
+  },
+  persist: {
+    key: 'vite-shop',
+    paths: ['token']
   }
 })
