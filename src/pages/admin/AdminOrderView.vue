214b143dec726dab4f03def52a06867a97e5809d
Mudi0710 <nomis820710@gmail.com>
2022-08-22 00:16:37 +0800

    修改-src/pages/admin/AdminOrderView.vue：修改訂單管理頁面
    修改訂單管理頁面

--- src/pages/admin/AdminOrderView.vue
+++ src/pages/admin/AdminOrderView.vue
@@ -1,196 +1,285 @@
 <template>
-  <div class='row justify-between'>
-    <div class='col-6 text-h3 text-secondary q-my-sm'>商品管理</div>
-    <q-btn v-if='!form.dialog' @click="openDialog('', -1)" square flat label='新增商品'
-      class='col-6 q-mr-xl bg-secondary text-dark text-h6 q-my-sm' style='width: 10%;'></q-btn>
-  </div>
-  <div class="q-pa-md">
-    <q-table title="Treats" :rows="products" :columns="columns" row-key="name" />
-  </div>
+  <q-page class='column'>
+    <div id="myheader" class="row justify-end items-center" style="width: 100%;">
+      <q-list class='text-h5 text-secondary mobile-none'>
+        <q-btn v-if='isLogin' round dense flat icon='fa-solid fa-house' to='/' class="q-mx-xs">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            回前台
+          </q-tooltip>
+        </q-btn>
+        <q-btn v-if='isLogin' round dense flat icon='fa-solid fa-right-from-bracket' @click='logout'
+          class="q-ml-xl q-mr-lg">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            登出
+          </q-tooltip>
+        </q-btn>
+      </q-list>
+    </div>
+    <div id='mycontent' class=''>
+      <div class='row justify-between'>
+        <div class='col-auto text-h3 text-secondary q-my-sm'>訂單管理</div>
+        <!-- <q-btn @click="openDialog('', -1)" square flat
+          class='col-auto q-mr-xl bg-secondary text-dark text-h6 q-my-sm createBtn'>新增商品</q-btn> -->
+      </div>
 
-  <q-card id="productForm" flat square bordered v-if="form.dialog" class="bg-primary text-secondary">
-    <q-form @submit.prevent='submitForm' class="q-pa-md" style="max-width: 400px">
-      <!-- 商品名稱 -->
-      <p>商品名稱：{{ form.name }}</p>
-      <q-input v-model="form.name" :rules='[rules.required]' type='text' outlined square dense />
-      <!-- 商品描述 -->
-      <p>商品描述：{{ form.description }}</p>
-      <q-input v-model="form.description" :rules='[rules.required]' type='text' outlined square dense />
-      <!-- 庫存狀態 -->
-      <p>庫存狀態：{{ form.inventory }}</p>
-      <q-toggle v-model="form.inventory" :label="form.inventory ? '有現貨' : '沒現貨'" />
-      <!-- 上架狀態 -->
-      <p>上架狀態：{{ form.sell }}</p>
-      <q-toggle v-model="form.sell" :label="form.inventory ? '上架' : '下架'" />
-      <!-- 商品價格 -->
-      <p>商品價格：{{ form.price }}</p>
-      <q-input v-model.number="form.price" min='0' :rules='[rules.required, rules.price]' outlined square dense />
-      <!-- 商品圖片 -->
-      <p>商品圖片：{{ form.image }}</p>
-      <q-file v-model='form.image' multiple :rules='[rules.size]' accept='image/*' filled bottom-slots counter>
-        <!-- 上傳icon -->
-        <template v-slot:prepend>
-          <q-icon name="cloud_upload" @click.stop.prevent />
-        </template>
-        <!-- 刪除icon -->
-        <template v-slot:append>
-          <q-icon name="close" @click.stop.prevent="model = null" class="cursor-pointer" />
-        </template>
+      <!-- <pre class="text-secondary">{{ orders }}</pre> -->
+      <!-- 訂單表單區 -->
+      <div class="col-12 q-pa-md" style="width: 100%;">
+        <q-table :grid="$q.screen.lt.xl" :columns="columns" :rows="orders" row-key="name" square bordered wrap-cells
+          binary-state-sort dense :filter="filter" :loading="loading" :pagination="pagination" no-data-label="目前沒有任何訂單"
+          rows-per-page-label="每頁顯示筆數" no-results-label="Oops...找不到該筆訂單">
 
-        <template v-slot:hint>
-        </template>
-      </q-file>
-      <!-- 確定新增 -->
-      <q-btn square flat type='submit' class="bg-secondary text-dark q-my-sm" label="新增商品" />
-      <!-- 取消新增 -->
-      <q-btn square flat outline class="bg-dark text-secondary q-my-sm" label="取消" @click='form.dialog = false'
-        v-close-popup />
-    </q-form>
+          <!-- 訂單搜尋 -->
+          <template v-slot:top-right>
+            <q-input borderless dense debounce="300" v-model="filter" placeholder="&nbsp;Search" class="search">
+              <template v-slot:append>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+          </template>
 
-  </q-card>
+          <!-- 訂單日期 -->
+          <template #body-cell-date="date">
+            <q-td :date="date" align="center">
+              <!-- <pre>{{ date.value }}</pre> -->
+              {{ new Date(date.value).toLocaleString() }}
+            </q-td>
+          </template>
+
+          <!-- 訂單金額 -->
+          <template #body-cell-totalPrice="totalPrice">
+            <q-td :totalPrice="totalPrice" align="center">
+              <!-- <pre>{{ totalPrice.row.totalPrice }}</pre> -->
+              NT$ {{ totalPrice.row.totalPrice.toLocaleString() }}
+            </q-td>
+          </template>
+
+          <!-- 訂單詳情 -->
+          <template #body-cell-detail="detail">
+            <q-td :detail="detail">
+              <div class="row justify-center">
+                <!-- <pre>{{ detail.row._id }}</pre> -->
+                <q-btn class="col-auto q-mx-sm" @click='openDetailDialog(detail)' outline>訂單詳情</q-btn>
+              </div>
+            </q-td>
+          </template>
+
+          <!-- 找不到資料的訊息 -->
+          <template v-slot:no-data="{ message }">
+            <div class="full-width row flex-center text-accent text-h5 q-gutter-sm">
+              <span>
+                {{ message }}
+              </span>
+            </div>
+          </template>
+
+          <!-- RWD 卡片 -->
+          <template v-slot:item="card">
+            <!-- <pre>{{ card.row.product.image }}</pre> -->
+            <div class="col-12 col-sm-6  col-md-4 col-lg-3 q-pa-sm cursor-pointer">
+              <q-card @click='openDialog(card.row._id, card.rowIndex)' square bordered class="bg-primary shadow"
+                style="100%">
+                <div v-for="col in card.cols" :key="col.name" class="q-pa-sm">
+                  <!-- <pre>{{ col }}</pre> -->
+                  <!-- 訂單日期 -->
+                  <div v-if="col.name === 'date'" class="text-left q-mx-auto">
+                    <div class="row justify-between items-center">
+                      <span class="text-accent">{{ col.label }}：</span>
+                      <span class="text-secondary text-body2 text-right ellipsis-3-lines">{{ new
+                        Date(col.value).toLocaleString()
+                        }}</span>
+                    </div>
+                  </div>
+                  <!-- 訂單id -->
+                  <div v-if="col.name === 'id'" class="text-left q-mx-auto">
+                    <div class="row justify-between">
+                      <span class="text-accent">{{ col.label }}：</span>
+                      <span class="text-secondary text-body2 text-right text-wrap">{{ col.value }}</span>
+                    </div>
+                  </div>
+                  <!-- 訂單id -->
+                  <div v-if="col.name === 'account'" class="text-left q-mx-auto">
+                    <div class="row justify-between">
+                      <span class="text-accent">{{ col.label }}：</span>
+                      <span class="text-secondary text-body2 text-right text-wrap">{{ col.value }}</span>
+                    </div>
+                  </div>
+                  <!-- 訂單金額 -->
+                  <div v-if="col.name === 'totalPrice'" class="text-left q-mx-auto">
+                    <div class="row justify-between">
+                      <span class="text-accent">{{ col.label }}：</span>
+                      <span class="text-secondary text-right ellipsis-3-lines">NT$ {{ col.value.toLocaleString()
+                        }}</span>
+                    </div>
+                  </div>
+                  <!-- 訂單詳情 -->
+                  <div v-if="col.name === 'detail'" class="text-left q-mx-auto">
+                    <div class="row justify-between">
+                      <span class="text-accent">{{ col.label }}：</span>
+                      <!-- <pre>{{ card }}</pre> -->
+                      <q-btn class="col-auto text-secondary" style="font-size: xx-small; padding: 0px 8px;"
+                        @click='openDetailDialog(card)' outline>訂單詳情</q-btn>
+                    </div>
+                  </div>
+
+                </div>
+              </q-card>
+            </div>
+          </template>
+
+          <!-- loading 效果 -->
+          <!-- QInnerLoading必须是其父元素内部的最后一个元素，以便它可以显示在其他内容的顶部。 -->
+          <template v-slot:loading>
+            <q-inner-loading showing dark transition-show="fade" color="primary" />
+          </template>
+        </q-table>
+      </div>
+
+      <!-- 訂單詳情的彈出視窗 -->
+      <q-dialog v-model="detailDialog.dialog" seamless style="width: auto;">
+        <q-card square class="row justify-center bg-info q-pa-lg" style="width: 100%;">
+          <div class="col-12 text-center text-h3 text-accent q-pb-md">訂單詳情</div>
+          <!-- 包一個框框 -->
+          <div class="row q-pa-md" style="width: 100%; border: 1px solid #1E1918;">
+
+            <!-- <pre>{{ detail }}</pre> -->
+            <!-- 訂單編號 -->
+            <div class="col-12 row">
+              <div class="col-12 col-sm-auto text-h6 text-accent">訂單編號：</div>
+              <div class="col-12 col-sm-auto text-sm-h6 text-dark">{{ detail.row._id }}</div>
+            </div>
+            <!-- 訂購會員 -->
+            <div class="col-12 row">
+              <div class="col-12 col-sm-auto text-h6 text-accent">訂購會員：</div>
+              <div class="col-12 col-sm-auto text-sm-h6 text-dark">{{ detail.row.user.account }}</div>
+            </div>
+            <!-- 訂單日期 -->
+            <div class="col-12 row">
+              <div class="col-12 col-sm-auto text-h6 text-accent">訂單日期：</div>
+              <div class="col-12 col-sm-auto text-sm-h6 text-dark">{{ new Date(detail.row.date).toLocaleString() }}
+              </div>
+            </div>
+
+            <!-- 分隔線 -->
+            <q-separator class="bg-dark text-center q-mx-auto q-my-sm" style="width: 95%; " />
+
+            <!-- 訂單詳情 -->
+            <div v-for="product in detail.row.products" :key="product._id" class="col-12 text-accent">
+              <div class="row">
+                <!-- 單一商品圖片 -->
+                <div class="col-12 col-lg-3">
+                  <q-responsive :ratio="3 / 2">
+                    <q-img :src="product.product.image[0]" />
+                  </q-responsive>
+                </div>
+                <!-- 單一商品內容 -->
+                <div class="col-12 col-lg-9 row q-px-lg-lg">
+                  <div class="col-12 row">
+                    <div class="col-12 col-sm-auto text-h6 text-accent">商品名稱：&nbsp;</div>
+                    <div class="col-12 col-sm text-sm-h6 text-dark">{{ product.product.name }}</div>
+                  </div>
+                  <div class="col-12 row">
+                    <div class="col-12 col-sm-auto text-h6 text-accent">商品金額：&nbsp; </div>
+                    <div class="col-12 col-sm text-sm-h6 text-dark">NT$ {{ product.product.price.toLocaleString() }}
+                    </div>
+                  </div>
+                  <div class="col-12 row">
+                    <div class="col-12 col-sm-auto text-h6 text-accent">訂購數量：&nbsp;</div>
+                    <div class="col-12 col-sm text-sm-h6 text-dark">{{ product.quantity }}</div>
+                  </div>
+                  <div class="col-12 text-h6 text-accent text-right">小計：&nbsp;<span class="text-dark">NT$
+                      {{ (product.product.price * product.quantity).toLocaleString() }}</span>
+                  </div>
+                </div>
+              </div>
+              <q-separator class="bg-dark text-center q-mx-auto q-my-sm" style="width: 95%; " />
+              <!-- <pre>{{ product }}</pre> -->
+            </div>
+            <!-- <pre>{{ detail.row.products }}</pre> -->
+            <!-- 商品總金額 -->
+            <div class="col-12 row justify-end q-px-lg-lg">
+              <div class="col-12 col-lg-auto text-h4 text-accent q-my-sm">訂單金額：</div>
+              <div class="col-12 col-lg-auto text-h4 text-warning text-right q-my-sm">NT$ {{
+                detail.row.totalPrice.toLocaleString()
+                }}</div>
+            </div>
+          </div>
+          <!-- 關閉按鈕 -->
+          <div class="col-12 row justify-around">
+            <q-btn square flat outline class="col-4 bg-dark text-secondary q-my-sm" label="關閉"
+              @click='detailDialog.dialog = false' />
+          </div>
+        </q-card>
+      </q-dialog>
+
+    </div>
+  </q-page>
 </template>
 
 <script setup>
 import { reactive, ref } from 'vue'
-import { apiAuth } from '../../boot/axios'
+import { apiAuth } from '@/boot/axios'
+import { storeToRefs } from 'pinia'
+import { useUserStore } from '@/stores/user'
 import { useQuasar } from 'quasar'
 import Swal from 'sweetalert2'
 
+const user = useUserStore()
+const { logout } = user
+const { isLogin } = storeToRefs(user)
+
 const filter = ref('')
 const $q = useQuasar()
-const products = reactive([])
 
-// 表單預設格式
-const form = reactive({
-  _id: '',
-  name: '',
-  description: '',
-  inventory: 'false',
-  sell: 'false',
-  price: 0,
-  image: [],
-  idx: -1,
+const pagination = reactive({
+  items: [], // table要顯示的資料
+  page: 1, // 目前第幾頁
+  rowsPerPage: 0 // 每頁幾筆，代表 All
+})
+
+const orders = reactive([])
+
+// 預設 訂單詳情的彈窗 為 false
+const detailDialog = reactive({
   dialog: false
 })
 
-// 驗證規則
-const rules = reactive({
-  required(v) {
-    return !!v || '必填欄位'
-  },
-  price(v) {
-    return (v > -1) || '價格必須大於或等於 0'
-  },
-  size(v) {
-    return !v || !v.length || (v[0]?.type?.includes('image') && v[0]?.size <= 1024 * 1024 * 2) || '檔案格式不符（須為圖片檔，且檔案大小 2 MB 以下）'
-  }
-})
+const detail = ref('')
 
-// 送出表單
-const submitForm = async () => {
-  // 表單送出時，因為後端要收到的是 FormData 的資料型態，所以要建立一個 FormData 物件
-  const fd = new FormData()
-  // 要把東西放進去 FormData 要使用 .append(key, value)，例如：fd.append('name', form.name)
-  // 可以一行一行寫，也可以一個 for 迴圈搞定
-  for (const key in form) {
-    if (['_id', 'idx', 'dialog', 'valid', 'submitting'].includes(key)) continue
-    // else if (key === 'image') {
-    //   for (const image of form.image) {
-    //     fd.append(key, image)
-    //   }
-    // }
-    else {
-      fd.append(key, form[key])
-    }
-  }
-  try {
-    if (form._id.length === 0) {
-      const { data } = await apiAuth.post('/products', fd)
-      products.push(data.result)
-      Swal.fire({
-        icon: 'success',
-        title: '新增成功',
-        text: '您已成功新增商品！'
-      })
-    } else {
-      const { data } = await apiAuth.patch('/products/' + form._id, fd)
-      products[form.idx] = data.result
-      Swal.fire({
-        icon: 'success',
-        title: '修改成功',
-        text: '您已成功修改商品！'
-      })
-    }
-    form.dialog = false
-  } catch (error) {
-    Swal.fire({
-      icon: 'error',
-      title: '新增失敗',
-      text: error.isAxiosError ? error.response.data.message : error.message
-    })
-  }
+/*
+  開啟 訂單詳情的彈窗，並帶入值
+  此時 detail.value = detail / card = orderId
+*/
+const openDetailDialog = (orderId) => {
+  detail.value = orderId
+  detailDialog.dialog = true
 }
 
-// 清空表單
-const openDialog = (_id) => {
-  const idx = _id === '' ? -1 : products.findIndex(product => product._id === _id)
-  form._id = _id
-  if (idx > -1) {
-    // 編輯
-    form.name = products[idx].name
-    form.description = products[idx].description
-    form.inventory = products[idx].inventory
-    form.sell = products[idx].sell
-    form.price = products[idx].price
-  } else {
-    // 清空
-    form.name = ''
-    form.description = ''
-    form.inventory = false
-    form.sell = false
-    form.price = 0
-  }
-  form.image = []
-  form.idx = idx
-  form.dialog = true
-}
-
-// const rows = reactive([{
-//   name: 'Frozen Yogurt',
-//   description: 'Frozen Yogurt',
-//   inventory: true,
-//   sell: true,
-//   price: 400
-// }])
+// 定義每列 title
 const columns = [
-  {
-    name: 'name',
-    label: '商品名稱',
-    field: row => row.name,
-    format: val => `${val}`,
-    // 是否凍結窗格
-    required: true,
-    align: 'left',
-    // 是否可以排序(A-Z or Z-A)
-    sortable: true
-  },
-  { name: 'description', label: '商品描述', field: row => row.description, align: 'left', sortable: true },
-  { name: 'inventory', label: '庫存狀態', field: row => row.inventory, align: 'center', sortable: true },
-  { name: 'sell', label: '上架狀態', field: row => row.sell, align: 'center', sortable: true },
-  { name: 'price', label: '商品價格', field: row => row.price, align: 'right', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+  { name: 'date', label: '訂單日期', field: row => row.date, required: true, align: 'center' },
+  { name: 'id', label: '訂單編號', field: row => row._id, required: true, align: 'center' },
+  { name: 'account', label: '訂購會員', field: row => row.user.account, required: true, align: 'center' },
+  { name: 'totalPrice', label: '訂單金額', field: row => row.totalPrice, align: 'center' },
+  { name: 'detail', label: '訂單詳情', align: 'center' }
 ]
 
-// 抓後台所有商品的資料
+// 抓使用者的訂單資料
 const init = async () => {
   try {
-    const { data } = await apiAuth.get('/products/all')
-    console.log(data)
-    products.push(...data.result)
+    const { data } = await apiAuth.get('/orders/all')
+    orders.push(...data.result.map(order => {
+      order.totalPrice = order.products.reduce((a, b) => {
+        return a + b.product.price * b.quantity
+      }, 0)
+      return order
+    }))
   } catch (error) {
+    console.log(error)
     Swal.fire({
       icon: 'error',
       title: '失敗',
-      text: error.isAxiosError ? error.response.data.message : error.message
+      text: '無法取得訂單'
     })
   }
 }
