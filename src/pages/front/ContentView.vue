1292c6718212338c32117ba8370ccd3e6de59a21
Mudi0710 <nomis820710@gmail.com>
2022-08-22 16:32:20 +0800

    修改-src/pages/front/ContentView.vue：修改聯絡我們頁面
    修改聯絡我們頁面

---
+++ src/pages/front/ContentView.vue
@@ -0,0 +1,82 @@
+<template>
+  <q-page>
+    <div id="myheader" class="row justify-end items-center" style="width: 100%;">
+      <q-list class='text-h5 text-secondary mobile-none'>
+        <q-btn v-if='isLogin' round dense flat icon='fa-solid fa-address-card' to='/member' class="q-mx-xs">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            會員資料
+          </q-tooltip>
+        </q-btn>
+        <q-btn v-if='isLogin' round dense flat icon='fa-regular fa-calendar-days' to='/booking' class="q-mx-xs">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            預約查詢
+          </q-tooltip>
+        </q-btn>
+        <q-btn v-if='isLogin' round dense flat icon='fa-solid fa-receipt' to='/order' class="q-mx-xs">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            訂單查詢
+          </q-tooltip>
+        </q-btn>
+        <q-btn v-if='isLogin' round dense flat icon='fa-solid fa-cart-shopping' to='/cart' class="q-mx-xs">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            購物車
+          </q-tooltip>
+          <q-badge v-if='cart > 0' floating color='red' rounded>{{ cart }}</q-badge>
+        </q-btn>
+        <q-btn v-if='isLogin && isAdmin' round dense flat icon='fa-solid fa-user-gear' to='/admin' class="q-mx-xs">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            管理後台
+          </q-tooltip>
+        </q-btn>
+        <q-btn v-if='!isLogin' round dense flat icon='fa-solid fa-user-plus' to='/register' class="q-mx-xs">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            註冊
+          </q-tooltip>
+        </q-btn>
+        <q-btn v-if='!isLogin' round dense flat icon='fa-solid fa-right-to-bracket' to='/login' class="q-ml-xs q-mr-sm">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            登入
+          </q-tooltip>
+        </q-btn>
+        <q-btn v-if='isLogin' round dense flat icon='fa-solid fa-right-from-bracket' @click='logout'
+          class="q-ml-xl q-mr-md">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            登出
+          </q-tooltip>
+        </q-btn>
+      </q-list>
+    </div>
+
+    <div id="mycontent" class="col row justify-start">
+
+      <!-- 頁面 Title -->
+      <div class="col-12 text-h3 text-secondary q-mb-md" style="width: 100%;">聯絡我們</div>
+
+      <!-- 麵包屑 -->
+      <div class="col-12 q-mt-md" style="width: 100%;">
+        <q-breadcrumbs>
+          <q-breadcrumbs-el label="首頁" icon="fa-solid fa-house" to="/" />
+          <q-breadcrumbs-el label="聯絡我們" icon="fa-solid fa-map-location-dot" />
+        </q-breadcrumbs>
+      </div>
+
+      <div class="col-12 q-mt-md" style="width: 100%;">
+
+      </div>
+
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+import { api } from '@/boot/axios'
+import { storeToRefs } from 'pinia'
+import { useUserStore } from '@/stores/user'
+import Swal from 'sweetalert2'
+
+const user = useUserStore()
+const { logout } = user
+const { isLogin, isAdmin, cart } = storeToRefs(user)
+
+</script>
