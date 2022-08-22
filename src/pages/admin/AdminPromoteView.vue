ed0082c8b0a64ecaec0bccb498e6509d5178e3e0
Mudi0710 <nomis820710@gmail.com>
2022-08-22 16:35:43 +0800

    修改-src/pages/admin/AdminPromoteView.vue：修改推播管理頁面
    修改推播管理頁面

---
+++ src/pages/admin/AdminPromoteView.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-page class='column'>
+    <div id="myheader" class="row justify-end items-center" style="width: 100%;">
+      <q-list class='text-h5 text-secondary mobile-none'>
+        <q-btn v-if='isLogin' round dense flat icon='fa-solid fa-house' to='/' class="q-mx-xs">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            回前台
+          </q-tooltip>
+        </q-btn>
+        <q-btn v-if='isLogin' round dense flat icon='fa-solid fa-right-from-bracket' @click='logout'
+          class="q-ml-xl q-mr-lg">
+          <q-tooltip transition-show='fade' transition-hide='fade' :offset='[0, 0]'>
+            登出
+          </q-tooltip>
+        </q-btn>
+      </q-list>
+    </div>
+    <div id='mycontent' class=''>
+      <div class='row justify-between'>
+        <div class='col-auto text-h3 text-secondary q-my-sm'>推播管理</div>
+        <!-- <q-btn @click="openDialog('', -1)" square flat
+          class='col-auto q-mr-xl bg-secondary text-dark text-h6 q-my-sm createBtn'>新增會員</q-btn> -->
+      </div>
+
+      <!-- 會員表格區 -->
+      <div class="q-pa-md">
+
+      </div>
+
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+import { api, apiAuth } from '@/boot/axios'
+import { storeToRefs } from 'pinia'
+import { useUserStore } from '@/stores/user'
+import { isEmail } from 'validator'
+import Swal from 'sweetalert2'
+
+const user = useUserStore()
+const { logout } = user
+const { isLogin } = storeToRefs(user)
+
+</script>
