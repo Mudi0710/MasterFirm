1ebc3df9721667bb2e5bfad759b827847b1f7cde
Mudi0710 <nomis820710@gmail.com>
2022-08-29 14:24:32 +0800

    修改-src/components/ServiceCard.vue：修改 服務項目 Card 元件
    修改 服務項目 Card 元件

--- src/components/ServiceCard.vue
+++ src/components/ServiceCard.vue
@@ -1,17 +1,17 @@
 <template>
-  <q-card class="row bg-transparent justify-center content-start no-shadow" style="border:none;border-radius: 0;"
+  <q-card class="row bg-info justify-center content-start no-shadow" style="border:none;border-radius: 0;"
     :service="service">
-    <div class="bg-info shadow-10" style="width: auto;">
+    <!-- <div class="bg-info shadow-10" style="width: auto;"> -->
       <!-- 服務 Icon -->
-      <q-card-section>
+      <q-card-section class="row justify-center">
         <div class="row justify-center content-center"
-          style="border: 5px solid #D09139; border-radius: 50%; width: 160px; height: 160px;">
-          <q-icon :name="service.icon" size="120px" color="accent" />
+          style="border: 5px solid #D09139; border-radius: 50%; width: 120px; height: 120px;">
+          <q-icon :name="service.icon" size="90px" color="accent" />
         </div>
       </q-card-section>
       <!-- 服務名稱 -->
       <q-card-section class="col-12 row justify-center no-padding">
-        <div class="text-h5 text-accent q-py-xs"> {{ service.name }} </div>
+        <div class="text-h5 text-accent text-center q-py-xs">{{ service.name }} </div>
       </q-card-section>
       <!-- 諮詢時間 -->
       <q-card-section class="col-12 row justify-center no-padding">
@@ -27,7 +27,7 @@
       <q-card-actions class="col-12 row justify-center no-padding">
         <q-btn square flat class="text-h6 bg-secondary text-dark q-mt-sm" :to="'/service/' + service._id" style="width: 100%;">服務詳情</q-btn>
       </q-card-actions>
-    </div>
+    <!-- </div> -->
   </q-card>
 </template>
 
